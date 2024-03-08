75460e4fd073fb3713e2785b55fc13157af46772
C Wael <wael.charni@outlook.fr>
2024-03-08 13:48:22 +0100

    Adding an if statement in the closeTaskFormBtn event listener. Adding an addOrUpdateTask arrow function and moving into it the dataArrIndex, taskObj and the if statement.

--- script/script.js
+++ script/script.js
@@ -12,6 +12,21 @@ const descriptionInput = document.getElementById("description-input");
 
 const taskData = [];
 let currentTask = {};
+
+const addOrUpdateTask = () => {
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    const taskObj = {
+        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value,
+      };
+
+      if (dataArrIndex === -1) {
+        taskData.unshift(taskObj);
+      };
+}
+
 const reset = () => {
   titleInput.value = "";
   dateInput.value = "";
@@ -25,8 +40,12 @@ openTaskFormBtn.addEventListener("click", () =>
 );
 
 closeTaskFormBtn.addEventListener("click", () => {
-  confirmCloseDialog.showModal();
   const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+  if (formInputsContainValues){
+    confirmCloseDialog.showModal();
+    } else {
+      reset();
+    }
 });
 
 cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
@@ -38,17 +57,6 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-    const taskObj = {
-        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-        title: titleInput.value,
-        date: dateInput.value,
-        description: descriptionInput.value,
-      };
-
-      if (dataArrIndex === -1) {
-        taskData.unshift(taskObj);
-      };
 
       taskData.forEach(({id, title, date, description}) => {
         (tasksContainer.innerHTML += `
